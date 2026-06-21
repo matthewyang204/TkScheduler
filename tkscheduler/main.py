b6b58c28fcf8ef243dadd2acacd348a778fd0176
Matthew Yang <matthew.yang204@gmail.com>
2026-06-21 10:41:43 -0700

    Add start & stop

--- tkscheduler/main.py
+++ tkscheduler/main.py
@@ -5,16 +5,33 @@ class TkScheduler:
         self.root = root
         self.id = None
         self.fn = None
+        self.selfScheduleId = None
 
         self.singleShotState = False
         self.interval = 0
-        self.active = False
 
     @staticmethod
     def singleShot(root, ms, fn):
         return root.after(ms, fn)
 
-    
+    def isActive(self):
+
+    def stop(self):
+        if self.id != None or self.selfScheduleId != None:
+            self.root.after_cancel(self.id)
+            self.root.after_cancel(self.selfScheduleId)
+            self.id = None
+            self.selfScheduleId = None
+        else:
+            print("WARNING: Job already cancelled or not started yet")
+
+    def start(self, ms):
+        if self.id != None:
+            print("WARNING: Not starting new job, job already in progress")
+            return
+
+        self.id = self.root.after(ms, self.fn)
+        self.selfScheduleId = self.root.after(ms, lambda: self.start(ms))
 
 def funnyCrash():
     print("This is a module and not a program. Stop running it as a program, it is not going to function as one.")
