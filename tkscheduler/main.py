cd24cb75fb26a7d95114fecf314f156afad3247b
Matthew Yang <matthew.yang204@gmail.com>
2026-06-20 18:15:30 -0700

    Initial commit

---
+++ tkscheduler/main.py
@@ -0,0 +1,6 @@
+import sys
+
+def funnyCrash():
+    print("This is a module and not a program. Stop running it as a program, it is not going to function as one.")
+    print("The correct way to use it is to import as a module for use in another program, so maybe go try that instead.")
+    sys.exit(2 * 4 * 8 * 16 * 32 * 64 * 128 * 256 * 512 * 1024 * 2048 * 4096 * 8192 * 16384)
